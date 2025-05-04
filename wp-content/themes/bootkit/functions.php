434249afcf1678c7b2c6645ce027eb1c390d7f7e
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 12:40:55 +0300

    Add my_action1

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -13,3 +13,15 @@ function my_filter_function2($str)
     return '<h2>Hello ' . $str . '!</h2>'; // Повертаємо змінене значення!
 }
 add_filter('my_filter2', 'my_filter_function2');
+
+function my_action_function()
+{
+    echo 'Сталася подія "my_action"!'; // Нічого не повертаємо!
+}
+add_action('my_action', 'my_action_function');
+
+function my_action_function1($text)
+{
+    echo 'Відбулася подія "my_action"!' . $text;
+}
+add_action('my_action1', 'my_action_function1');
