63d2d206a9016b7e3275499d2ec252d75d31f54c
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 12:45:15 +0300

    Add 123

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -22,6 +22,14 @@ add_action('my_action', 'my_action_function');
 
 function my_action_function1($text)
 {
-    echo 'Відбулася подія "my_action"!' . $text;
+    echo 'Відбулася подія "my_action"!' . $text . "<br>";
 }
 add_action('my_action1', 'my_action_function1');
+
+
+// До одного хука можна прикріпити кілька різних функцій
+// Тут функції задані як анонімні
+add_action( 'my_hook', function(){ echo 1; }, 3 );
+add_action( 'my_hook', function(){ echo 2; }, 2 );
+add_action( 'my_hook', function(){ echo 3; }, 1 );
+do_action('my_hook'); // 123
