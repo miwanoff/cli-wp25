13e8c372233d81025de3dfc9c3829788ac12c05e
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 13:03:02 +0300

    Add assets

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -7,6 +7,6 @@
 include get_theme_file_path('includes/enqueue.php');
 
 // Hooks
-
+//add_action('wp_enqueue_scripts', 'bootkit_enqueue');
 
 // Shortcodes
--- wp-content/themes/bootkit/includes/enqueue.php
+++ wp-content/themes/bootkit/includes/enqueue.php
@@ -1,2 +1,9 @@
 <?php
-echo 'Hello';
+
+function bootkit_enqueue()
+{
+    $url = get_theme_file_uri();
+    wp_register_style('bootkit_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); // Google fonts
+    wp_register_style('bootkit_bootstrap', $url . '/assets/vendor/bootstrap/css/bootstrap.min.css'); //bootstrap.min.css
+    wp_register_style('bootkit_modern_business', $url . '/assets/css/modern-business.css'); // modern-business.css
+}
