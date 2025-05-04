658ec209eaf2df3de1c568bed7d70410ba2bf65f
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 12:36:55 +0300

    Add my_filter2

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -2,8 +2,14 @@
 
 function my_filter_function1($str)
 {
-    $str = '<strong>'. $str. '</strong>';
+    $str = '<strong>' . $str . '</strong>';
     return $str; // Повертаємо змінене значення!
 }
 
 add_filter('my_filter1', 'my_filter_function1');
+
+function my_filter_function2($str)
+{
+    return '<h2>Hello ' . $str . '!</h2>'; // Повертаємо змінене значення!
+}
+add_filter('my_filter2', 'my_filter_function2');
--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -1,5 +1,7 @@
 <?php
 wp_head(); // тут автоматично буде підключено файл functions.php
-
+echo "<h1>WP</h1>";
 $str      = 'Kit';
 echo $str = apply_filters('my_filter1', $str); // <strong>Kit</strong>
+
+echo apply_filters('my_filter2', $str); // Hello Kit
