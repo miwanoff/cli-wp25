006d184acef7c49b63874fdaacfebd1ea720a468
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 12:56:06 +0300

    Add includes/enqueue.php

--- wp-content/themes/bootkit/functions.php
+++ wp-content/themes/bootkit/functions.php
@@ -1,35 +1,12 @@
 <?php
 
-function my_filter_function1($str)
-{
-    $str = '<strong>' . $str . '</strong>';
-    return $str; // Повертаємо змінене значення!
-}
-
-add_filter('my_filter1', 'my_filter_function1');
-
-function my_filter_function2($str)
-{
-    return '<h2>Hello ' . $str . '!</h2>'; // Повертаємо змінене значення!
-}
-add_filter('my_filter2', 'my_filter_function2');
-
-function my_action_function()
-{
-    echo 'Сталася подія "my_action"!'; // Нічого не повертаємо!
-}
-add_action('my_action', 'my_action_function');
-
-function my_action_function1($text)
-{
-    echo 'Відбулася подія "my_action"!' . $text . "<br>";
-}
-add_action('my_action1', 'my_action_function1');
+// Setup
 
 
-// До одного хука можна прикріпити кілька різних функцій
-// Тут функції задані як анонімні
-add_action( 'my_hook', function(){ echo 1; }, 3 );
-add_action( 'my_hook', function(){ echo 2; }, 2 );
-add_action( 'my_hook', function(){ echo 3; }, 1 );
-do_action('my_hook'); // 123
+// Includes
+include get_theme_file_path('includes/enqueue.php');
+
+// Hooks
+
+
+// Shortcodes
--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -1,11 +1,2 @@
 <?php
 wp_head(); // тут автоматично буде підключено файл functions.php
-echo "<h1>WP</h1>";
-$str      = 'Kit';
-echo $str = apply_filters('my_filter1', $str); // <strong>Kit</strong>
-
-echo apply_filters('my_filter2', $str); // Hello Kit
-
-do_action('my_action'); // Відбулася подія "my_action"!
-
-do_action('my_action1', 'Hello!'); // Відбулася подія "my_action"! Hello!
