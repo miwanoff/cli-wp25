d009e3c4aaa519b5f83ea704a4f4cc1ba9ef664c
miwanoff <marina.iva.noffa@gmail.com>
2025-05-04 13:23:45 +0300

    Add slider

--- wp-content/themes/bootkit/includes/enqueue.php
+++ wp-content/themes/bootkit/includes/enqueue.php
@@ -10,6 +10,7 @@ function bootkit_enqueue()
     wp_enqueue_style('bootkit_bootstrap');
     wp_enqueue_style('bootkit_modern_business');
     wp_register_script('bootkit_bootstrap', $url .
-        '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', [], false, true);
+        '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js', 'jquery', false, true);
+    wp_enqueue_script('jquery');
     wp_enqueue_script('bootkit_bootstrap');
 }
