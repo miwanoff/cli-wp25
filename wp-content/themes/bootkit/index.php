1b69c1baa79c8d2c8d391181fc433e3b575f8ed9
miwanoff <marina.iva.noffa@gmail.com>
2025-05-11 13:05:37 +0300

    Add post excerpt

--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -43,7 +43,7 @@ get_header(); ?>
             <?php if (have_posts()) {
                     while (have_posts()) {
                         the_post();
-                        get_template_part('partials/posts/content-excerpt');
+                        get_template_part('partials/posts/content', 'excerpt');
                     }
                 }
             ?>
--- wp-content/themes/bootkit/partials/posts/content-excerpt.php
+++ wp-content/themes/bootkit/partials/posts/content-excerpt.php
@@ -1,15 +1,21 @@
             <!-- Blog Post -->
             <div class="card mb-4">
-                <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
+                <!-- <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap"> -->
+                <?php
+
+                    if (has_post_thumbnail()) {
+                        the_post_thumbnail("full", ["class" => "card-img-top"]);
+                    }
+
+                ?>
                 <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid
-                        atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero
-                        voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More →</a>
+                    <h2 class="card-title"><a href="<?php the_permalink()?>"><?php the_title()?></a></h2>
+                    <p><?php the_category(" ")?></p>
+                    <p class="card-text"><?php the_excerpt()?></p>
+                    <a href="<?php the_permalink()?>" class="btn btn-primary">Read More →</a>
                 </div>
                 <div class="card-footer text-muted">
-                    Posted on January 1, 2020 by
-                    <a href="#">Start Bootstrap</a>
+                    Posted on <?php echo get_the_date() ?>by
+                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"><?php the_author()?></a>
                 </div>
             </div>
