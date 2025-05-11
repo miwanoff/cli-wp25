ec8f1dfa0a788c4bfd97c67acfea2a849cf4c96f
miwanoff <marina.iva.noffa@gmail.com>
2025-05-11 12:49:24 +0300

    Add content-excerpt

--- wp-content/themes/bootkit/index.php
+++ wp-content/themes/bootkit/index.php
@@ -1,5 +1,5 @@
 <?php
-get_header();?>
+get_header(); ?>
 
 <!-- Page Content -->
 <div class="container">
@@ -8,7 +8,7 @@ get_header();?>
 
     <!-- Marketing Icons Section -->
     <div class="row">
-        <div class="col-lg-4 mb-4">
+        <!-- <div class="col-lg-4 mb-4">
             <div class="card h-100">
                 <h4 class="card-header">Card Title</h4>
                 <div class="card-body">
@@ -32,9 +32,34 @@ get_header();?>
                     <a href="#" class="btn btn-primary">Learn More</a>
                 </div>
             </div>
+        </div> -->
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+            <h1 class="my-4">Page Heading
+                <small>Secondary Text</small>
+            </h1>
+
+
+            <?php if (have_posts()) {
+                    while (have_posts()) {
+                        the_post();
+                        get_template_part('partials/posts/content-excerpt');
+                    }
+                }
+            ?>
+
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item">
+                    <a class="page-link" href="#">← Older</a>
+                </li>
+                <li class="page-item disabled">
+                    <a class="page-link" href="#">Newer →</a>
+                </li>
+            </ul>
         </div>
-        <?php get_sidebar();?>
-        <?php /* get_sidebar('second')  sidebar-second.php */ ?>
+        <?php get_sidebar(); ?>
+        <?php /* get_sidebar('second')  sidebar-second.php */?>
     </div>
     <!-- /.row -->
 
@@ -42,4 +67,4 @@ get_header();?>
 </div>
 <!-- /.container -->
 
-<?php get_footer();?>
+<?php get_footer(); ?>
