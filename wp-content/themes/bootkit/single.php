929d0b5ef5c0538f587b789f58ae591e79500e65
miwanoff <marina.iva.noffa@gmail.com>
2025-05-17 13:38:19 +0300

    Add WP_Query

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -54,7 +54,7 @@ get_header(); ?>
                 <div class="card-header">
                     <strong>
                         Posted by
-                        <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+                        <a href="<?php echo $author_URL; ?>"><?php the_author(); ?></a>
                     </strong>
                 </div>
                 <div class="card-body">
@@ -69,7 +69,55 @@ get_header(); ?>
             <!-- Tag cloud -->
             <?php the_tags('', ', '); ?>
             <hr>
+            <h4>Related Posts:</h4>
+            <div class="related-posts clearfix">
+                <?php
+                    $categories = get_the_category();
+                            $rp_query   = new WP_Query([
+                                'posts_per_page' => 2,
+                                'post__not_in'   => [$post->ID],
+                                'cat'            => ! empty($categories) ? $categories[0]->term_id : null,
+                            ]);
+                            if ($rp_query->have_posts()) {
+                                while ($rp_query->have_posts()) {
+                                    $rp_query->the_post();
+                                ?>
+                <div class="mpost clearfix">
+                    <?php
+                        if (has_post_thumbnail()) {
+                                        ?>
+                    <div class="entry-image">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </a>
+                    </div>
+                    <?php
+                        }
+                                    ?>
+                    <div class="entry-c">
+                        <div class="entry-title">
+                            <h4>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+                        </div>
+                        <ul class="entry-meta clearfix">
+                            <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+                            <li><i class="icon-comments"></i> <?php comments_number('0'); ?></li>
+                        </ul>
+                        <!-- <div class="entry-content">
+                            <?php the_excerpt(); ?>
+                        </div> -->
+                    </div>
+                </div>
+                <?php
+                    }
+                                wp_reset_postdata();
 
+                            }
+                        ?>
+            </div>
             <!-- Pagination -->
             <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
@@ -82,8 +130,8 @@ get_header(); ?>
             <?php
                 if (comments_open() || get_comments_number()) {
                             comments_template();
-                        }                   
-                }
+                        }
+                    }
             }?>
         </div>
         <?php get_sidebar(); ?>
