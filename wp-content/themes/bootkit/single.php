ac4e3d856401af089e4cc606a07b6bca8f00de0f
miwanoff <marina.iva.noffa@gmail.com>
2025-05-18 12:17:29 +0300

    Add News

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -69,12 +69,57 @@ get_header(); ?>
             <!-- Tag cloud -->
             <?php the_tags('', ', '); ?>
             <hr>
+            <h4>News:</h4>
+            <div class="related-posts clearfix">
+                <?php
+                    $categories = get_the_category();
+                            $rp_query   = new WP_Query(['category_name' => 'news', 'post__not_in' => [$post->ID]]);
+                            if ($rp_query->have_posts()) {
+                                while ($rp_query->have_posts()) {
+                                    $rp_query->the_post();
+                                ?>
+                <div class="mpost clearfix">
+                    <?php
+                        if (has_post_thumbnail()) {
+                                        ?>
+                    <div class="entry-image">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </a>
+                    </div>
+                    <?php
+                        }
+                                    ?>
+                    <div class="entry-c">
+                        <div class="entry-title">
+                            <h4>
+                                <a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h4>
+                        </div>
+                        <ul class="entry-meta clearfix">
+                            <li><i class="icon-calendar3"></i>                                                               <?php echo get_the_date(); ?></li>
+                            <li><i class="icon-comments"></i>                                                              <?php comments_number('0'); ?></li>
+                        </ul>
+                        <!-- <div class="entry-content">
+                            <?php the_excerpt(); ?>
+                        </div> -->
+                    </div>
+                </div>
+                <?php
+                    }
+                                wp_reset_postdata();
+
+                            }
+                        ?>
+            </div>
             <h4>Related Posts:</h4>
             <div class="related-posts clearfix">
                 <?php
                     $categories = get_the_category();
                             $rp_query   = new WP_Query([
-                                'posts_per_page' => 2,
+                                'posts_per_page' => 4,
                                 'post__not_in'   => [$post->ID],
                                 'cat'            => ! empty($categories) ? $categories[0]->term_id : null,
                             ]);
@@ -103,8 +148,8 @@ get_header(); ?>
                             </h4>
                         </div>
                         <ul class="entry-meta clearfix">
-                            <li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
-                            <li><i class="icon-comments"></i> <?php comments_number('0'); ?></li>
+                            <li><i class="icon-calendar3"></i>                                                               <?php echo get_the_date(); ?></li>
+                            <li><i class="icon-comments"></i>                                                              <?php comments_number('0'); ?></li>
                         </ul>
                         <!-- <div class="entry-content">
                             <?php the_excerpt(); ?>
@@ -118,6 +163,7 @@ get_header(); ?>
                             }
                         ?>
             </div>
+
             <!-- Pagination -->
             <ul class="pagination justify-content-center mb-4">
                 <li class="page-item">
@@ -135,7 +181,7 @@ get_header(); ?>
             }?>
         </div>
         <?php get_sidebar(); ?>
-        <?php /* get_sidebar('second')  sidebar-second.php */?>
+<?php /* get_sidebar('second')  sidebar-second.php */?>
     </div>
     <!-- /.row -->
 
