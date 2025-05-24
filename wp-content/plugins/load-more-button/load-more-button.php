3dedc7bc2d0a844c4d199d0b043a31c54897bc12
miwanoff <marina.iva.noffa@gmail.com>
2025-05-24 13:37:35 +0300

    Add template

--- wp-content/plugins/load-more-button/load-more-button.php
+++ wp-content/plugins/load-more-button/load-more-button.php
@@ -27,3 +27,34 @@ function create_button_and_script()
 }
 
 add_action('get_footer', 'create_button_and_script');
+
+function bootkit_load_more_scripts()
+{
+    // In most cases it is already included on the page and this line can be removed
+    wp_enqueue_script('jquery');
+    // register наш main script but do not enqueue it yet
+    wp_register_script('loadmore', plugin_dir_url(__FILE__) . 'js/loadmore.js', array('jquery'));
+    wp_enqueue_script('loadmore');
+}
+add_action('wp_enqueue_scripts', 'bootkit_load_more_scripts');
+
+function bootkit_load_posts()
+{
+    $args = unserialize(stripslashes($_POST['query'])); // Запит від скрипту
+    $args['paged'] = $_POST['page'] + 1; // номер сторінки
+    $args['post_status'] = 'publish'; // тільки опубліковані
+    $args['posts_per_page'] = get_option('posts_per_page'); // скільки постів на сторінці (підвантажувати по 2)
+    // визначаємо, які посади будуть показані в базовому циклі WordPress
+    query_posts($args);
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+        //    echo "<h2>".the_title()."</h2>";
+         include "templates/template.php";
+        }
+    }
+    die();
+}
+
+add_action('wp_ajax_loadmore', 'bootkit_load_posts');
+add_action('wp_ajax_nopriv_loadmore', 'bootkit_load_posts');
